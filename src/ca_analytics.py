356a9e159d61d238940bf5d74a40d459ec0aec6a
Alek <alekqwas@poczta.fm>
2016-12-08 01:06:54 +0100

    Clenup action for release

--- src/ca_analytics.py
+++ src/ca_analytics.py
@@ -6,10 +6,9 @@ import logging
 import os
 import sys
 from os.path import join as j
-from pprint import pprint
 
-from lib.engine import get_ca_event_list
 from lib.database import init_db
+from lib.engine import get_ca_event_list
 from lib.extras import configure_argparse, Setts, OutputHandler, timeit
 
 rwd = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
@@ -27,20 +26,16 @@ def evaluate_arguments():
                                                 date_to=Setts.DATE_TO.value)
 
     event_list = get_ca_event_list(selected_logs=db_data)
-    # pprint(event_list)
 
-    # printer = OutputHandler(ca_events_list=ca_event_list)
-    # if Setts.OUT_DEST.value:
-    #     printer.write_file(f_path=Setts.OUT_DEST.value)
-    # else:
-    #     printer.write_terminal()
+    printer = OutputHandler(ca_events_list=event_list)
+    if Setts.OUT_DEST.value:
+        printer.write_file(f_path=Setts.OUT_DEST.value)
+    else:
+        printer.write_terminal()
 
 
 @timeit
 def main(start_cmd=None):
-    # start_cmd = '-u 109822325191351771849'.split()
-    # start_cmd = '-e 321 430'.split()
-    # start_cmd = '-e 321 '.split()
     args, parser = configure_argparse(rwd=rwd, start_cmd=start_cmd)
 
     # Load default cfg
@@ -54,12 +49,11 @@ def main(start_cmd=None):
     evaluate_arguments()
 
 
-version = {'y': 2016, 'm': 12, 'd': 2}
+version = {'y': 2016, 'm': 12, 'd': 8}
 __version__ = '{y}.{m}.{d}'.format(**version)
 
 if __name__ == '__main__':
     # TODO: Decorator for properties, when _field is not None return earlier
-    # TODO: Sane error messages when no details for event/user
     try:
         main()
     except Exception as e:
