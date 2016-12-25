febb633cd9315f5e01b6f1e99f23323cb1a8c6b6
Alek <alekqwas@poczta.fm>
2016-12-26 00:48:13 +0100

    Create stub for date tests

---
+++ test/test_date_selections.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    def test_should_filter_out_upper_bound(self):
+        pass
+
+    def test_should_filter_out_lower_bound(self):
+        pass
+
+    def test_should_include_upper_bound(self):
+        pass
+
+    def test_should_include_lower_bound(self):
+        pass
+
+    def test_should_omit_just_before_upper_bound(self):
+        pass
+
+    def test_should_omit_just_after_lower_bound(self):
+        pass
+
+    def test_should_include_up_and_low_when_period_given(self):
+        pass
+
+    def test_should_raise_an_exception_when_incorrect_data(self):
+        pass
